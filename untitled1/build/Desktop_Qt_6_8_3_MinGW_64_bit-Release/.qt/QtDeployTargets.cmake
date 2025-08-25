6ba1f762a2f60b3cdb87ddc60cac897da78b9a64
mhosseini <mahdieh8hosseini@gmail.com>
2025-08-25 22:26:46 +0330

    fix: remove extra files for build

--- untitled1/build/Desktop_Qt_6_8_3_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled1_FILE C:/Users/Mahdieh-hosseini/Documents/untitled1/build/Desktop_Qt_6_8_3_MinGW_64_bit-Release/untitled1.exe)
-set(__QT_DEPLOY_TARGET_untitled1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled1_RUNTIME_DLLS D:/QT/6.8.3/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.8.3/mingw_64/bin/Qt6Gui.dll;D:/QT/6.8.3/mingw_64/bin/Qt6Core.dll)
